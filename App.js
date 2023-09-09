a7d9317b1dcd23d0979ba8fddf5eea39a87e6519
sam <aquaticcalf@proton.me>
2023-09-09 12:13:38 +0530

    meow

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Loginscreen from './screens/loginscreen'
+import Signupscreen from './screens/signupscreen'
+import Homescreen from './screens/homescreen'
+
+
+const Stack = createNativeStackNavigator()
+
+const globalScreenOptions ={
+  headerShown: false
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen name='login' component={Loginscreen} />
+        <Stack.Screen name='signup' component={Signupscreen}/>
+        <Stack.Screen name='home' component={Homescreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
