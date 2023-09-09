0bd7d7c4cb8f68f2e43a8cac8acc9eb0d236239a
M Guru Nikhil <130775801+MGuruNikhil@users.noreply.github.com>
2023-09-09 14:17:15 +0530

    meow

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
     apiKey: "AIzaSyBMANBoTHkV3UGU_DxGtx2pHDG7Apaqfr8",
     authDomain: "togo-ee66d.firebaseapp.com",
@@ -7,3 +10,7 @@ const firebaseConfig = {
     appId: "1:120161114525:web:7954b5ef3fa8d2021a7774",
     measurementId: "G-1SBL8DC9GP"
 }
+if(!getApps().length) {
+    const app = initializeApp(firebaseConfig);
+}
+export const auth = getAuth(app);
