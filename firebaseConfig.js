7a3c23603d778b0770f3050e29762f42ce6590a2
sam <aquaticcalf@proton.me>
2023-09-09 13:08:31 +0530

    meow

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBMANBoTHkV3UGU_DxGtx2pHDG7Apaqfr8",
+    authDomain: "togo-ee66d.firebaseapp.com",
+    projectId: "togo-ee66d",
+    storageBucket: "togo-ee66d.appspot.com",
+    messagingSenderId: "120161114525",
+    appId: "1:120161114525:web:7954b5ef3fa8d2021a7774",
+    measurementId: "G-1SBL8DC9GP"
+}
---
+++ screens/signupscreen.js
@@ -0,0 +1,64 @@
+import { StyleSheet, Text ,SafeAreaView} from 'react-native'
+import React, { useState } from 'react'
+import { TextInput, Button } from 'react-native-paper'
+
+
+
+const Signupscreen = ({navigation}) => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  return(
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Sign up for toGo</Text>
+      <TextInput
+        style={styles.input}
+        label="full name"
+        value={name}
+        onChangeText={text => setName(text)}
+      />    
+      <TextInput
+        style={styles.input}
+        label="email"
+        value={email}
+        onChangeText={text => setEmail(text)}
+      />    
+      <TextInput
+        style={styles.input}
+        label="password"
+        value={password}
+        secureTextEntry
+        onChangeText={text => setPassword(text)}
+      />
+      <Button style={styles.button} mode="contained">
+        Press me
+      </Button>      
+      <Button style={styles.button} mode="outlined">
+        signup
+      </Button>
+  </SafeAreaView>
+)}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems:'center',
+    justifyContent: 'center',
+  },
+  title:{
+    fontSize:40,
+    margin:10,
+  },
+  input:{
+    width:300,
+    margin:10,
+  },
+  button:{
+    width: 200,
+    marginTop: 10,
+    marginBottom: 2,
+  }
+})
+
+
+export default Signupscreen
